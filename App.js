a83a10372e4a5fc310d69eba694341d364f7c247
dulyts <jobb.gabor@gmail.com>
2024-06-20 18:15:49 +0200

    issue repro

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from "expo-notifications";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import {
+    Alert,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    useEffect(() => {
+        const sub = Notifications.addPushTokenListener((token) => {
+            console.log("pushTokenListener called");
+            Alert.alert(
+                "pushTokenListener called",
+                `token: ${JSON.stringify(token)}`
+            );
+        });
+        return () => sub?.remove();
+    });
+    const requestPushToken = async () => {
+        const token = await Notifications.getDevicePushTokenAsync();
+        console.log("getDevicePushTokenAsync response", token);
+    };
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <TouchableHighlight onPress={requestPushToken} style={styles.btn}>
+                <Text>RequestPushToken</Text>
+            </TouchableHighlight>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    btn: {
+        padding: 10,
+        backgroundColor: "lightblue",
+    },
 });
